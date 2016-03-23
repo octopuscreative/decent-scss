da06ea5bea9996d49ce1517839531ce9806a4eb3
Sean Washington <hello@seanwash.com>
2016-03-22 17:03:32 -0700

    A bit of cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,11 @@ var autoPrefixBrowserList = ['last 2 version'];
 
 var gulp          = require('gulp'),
     gutil         = require('gulp-util'),
-    coffee        = require('gulp-coffee'),
     concat        = require('gulp-concat'),
     uglify        = require('gulp-uglify'),
     sass          = require('gulp-sass'),
     sassGlob      = require('gulp-sass-glob'),
     sourceMaps    = require('gulp-sourcemaps'),
-    imagemin      = require('gulp-imagemin'),
     minifyCSS     = require('gulp-minify-css'),
     browserSync   = require('browser-sync'),
     autoprefixer  = require('gulp-autoprefixer'),
@@ -19,49 +17,14 @@ var gulp          = require('gulp'),
 
 gulp.task('browserSync', function() {
   browserSync({
-    server: { baseDir: "src/" },
+    server: { baseDir: "./" },
     reloadDelay: 1000,
     notify: false
   });
 });
 
-gulp.task('images', function(tmp) {
-  gulp.src(['src/images/*.jpg', 'src/images/*.png'])
-    .pipe(plumber())
-    .pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
-    .pipe(gulp.dest('src/images'));
-});
-
-gulp.task('coffee', function() {
-  gulp.src('./src/scripts/**/*.coffee')
-  .pipe(coffee({bare: true})
-  .on('error', gutil.log))
-  // .pipe(concat('app.js'))
-  .pipe(gulp.dest('./src/scripts/compiled'));
-  // .pipe(browserSync.reload({stream: true}));
-});
-
-gulp.task('scripts', ['coffee'], function() {
-  return gulp.src([
-    './src/scripts/vendor/jquery.min.js',
-    './src/scripts/vendor/timelinemax.min.js',
-    './src/scripts/vendor/tweenmax.min.js',
-    './src/scripts/vendor/scrollmagic.min.js',
-    './src/scripts/vendor/jquery.scrollmagic.min.js',
-    './src/scripts/vendor/gsap.scrollmagic.min.js',
-    './src/scripts/vendor/jquery.gsap.min.js',
-    // './src/scripts/vendor/animation.gsap.js',
-    './src/scripts/vendor/ScrollToPlugin.js',
-    './src/scripts/compiled/**/*.js'
-    ])
-    .pipe(concat('app.js'))
-    .on('error', gutil.log)
-    .pipe(gulp.dest('src/scripts'))
-    .pipe(browserSync.reload({stream: true}));
-});
-
 gulp.task('styles', function() {
-  return gulp.src('src/stylesheets/scss/init.scss')
+  return gulp.src('src/scss/init.scss')
     .pipe(plumber({
       errorHandler: function (err) {
         console.log(err);
@@ -72,7 +35,7 @@ gulp.task('styles', function() {
     .pipe(sassGlob())
     .pipe(sass({
       errLogToConsole: true,
-      includePaths: [ 'src/stylesheets/scss/' ]
+      includePaths: [ 'src/scss/' ]
     }))
     .pipe(autoprefixer({
       browsers: autoPrefixBrowserList,
@@ -81,7 +44,7 @@ gulp.task('styles', function() {
     .on('error', gutil.log)
     .pipe(concat('styles.css'))
     .pipe(sourceMaps.write())
-    .pipe(gulp.dest('src/stylesheets'))
+    .pipe(gulp.dest('src'))
     .pipe(browserSync.reload({stream: true}));
 });
 
@@ -93,14 +56,14 @@ gulp.task('html', function() {
     .on('error', gutil.log);
 });
 
-gulp.task('clean-dist', shell.task([ 
-  'rm -rf ./dist' 
+gulp.task('clean-dist', shell.task([
+  'rm -rf ./dist'
 ]));
 
 // Copy the util stylesheets to a distribution folder.
 gulp.task('copy-util', ['clean-dist'], function() {
   gulp.src([
-    './src/stylesheets/scss/**/*'
+    './src/scss/**/*'
   ])
   .pipe(gulp.dest('./dist'));
 });
@@ -113,9 +76,9 @@ gulp.task('copy-util', ['clean-dist'], function() {
 //  - watch files.
 //  - copy files to the dist folder.
 
-gulp.task('default', ['scripts', 'styles', 'browserSync', 'copy-util'], function() {
+gulp.task('default', ['styles', 'browserSync', 'copy-util'], function() {
   gulp.watch(['src/scripts/src/vendor/**', 'src/scripts/**/*.coffee'], ['scripts']);
-  gulp.watch('src/stylesheets/scss/**', ['styles', 'copy-util']);
+  gulp.watch('src/scss/**', ['styles', 'copy-util']);
   gulp.watch('src/images/**', ['images']);
   gulp.watch('src/*.html', ['html']);
   gulp.watch()
